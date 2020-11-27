5674afbf4e24b025c88472c33628fb383a1d48d3
kdh <do.hyeon.lioncco.com>
2020-11-27 16:25:10 +0900

    :truck: 유틸함수 파일 분리

---
+++ src/utils.ts
@@ -0,0 +1,40 @@
+const DEVICE_EVENT_NAMES = {
+  mobile: {
+    start: 'touchstart',
+    move: 'touchmove',
+    end: 'touchend'
+  },
+  desktop: {
+    start: 'mousedown',
+    move: 'mousemove',
+    end: 'mouseup'
+  }
+}
+
+type GetIsMobile = () => boolean;
+export const getIsMobile: GetIsMobile = () => {
+  if(!(process as any).browser) {
+    return false;
+  }
+  return /iPhone|iPad|iPod|Android/i.test(navigator?.userAgent ?? '');
+}
+
+type getAbsolutePositionFunc = (element: HTMLElement) => (position: 'x' | 'y') => number;
+export const getAbsolutePositionFunc: getAbsolutePositionFunc = (element) => (position) => {
+  return (
+    window[`scroll${position.toUpperCase()}`] +
+    element.getBoundingClientRect()[position]
+  );
+};
+
+
+type GetEventNameByDevice = (isMobile: boolean) => {
+  start: 'string';
+  move: 'string';
+  end: 'string';
+};
+// @ts-ignore
+export const getEventNameByDevice: GetEventNameByDevice = (isMobile) => {
+  return DEVICE_EVENT_NAMES[isMobile ? 'mobile' : 'desktop'];
+}
+
