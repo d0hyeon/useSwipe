d2abaecf92f9de29d53800d5413a683f3c674031
d0hyeon <ehgus6887@gmail.com>
2021-10-06 00:59:10 +0900

    build: cjs 모듈 제거  - react hooks 이기 때문에 노드에서 실행될 가능성 없다고 판단

--- rollup.config.js
+++ rollup.config.js
@@ -8,18 +8,11 @@ import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-    }, 
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-    }
-  ],
+  output: {
+    file: pkg.main,
+    format: 'es',
+    exports: 'named',
+  }, 
   plugins: [
     resolve({
       preferBuiltins: false
